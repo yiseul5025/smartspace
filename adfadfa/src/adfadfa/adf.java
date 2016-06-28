077ef6e378d9f4dc1dbbbe12f89e1d4b69ba5da2
309 <309@309-002-PC>
2016-06-28 20:38:44 +0900

    7uuuhik

---
+++ adfadfa/src/adfadfa/adf.java
@@ -0,0 +1,17 @@
+package adfadfa;
+
+public class adf {public static void main(String[] args) {
+	System.out.println("hello");
+	System.out.println("신유나 바보");
+	System.out.println("신유나 바보");
+	System.out.print("신유나");
+	System.out.println(" 바보");
+	System.out.println("신유나 는 바아아아아보 ");
+	for(int i=0;i<10;i++){
+		
+		System.out.println("신유나는 바보이다");
+	}
+	
+}
+
+}
